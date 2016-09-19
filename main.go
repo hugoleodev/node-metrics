7541f161e90a742cd702dbd4b79974e05129d008
Hugo Leonardo Costa e Silva <hugoleodev@gmail.com>
2016-09-19 09:48:16 -0300

    Criando o ResourceMetric como struct e coletando informações de rede, disco e CPU

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/fatih/structs"
 	"github.com/influxdata/influxdb/client/v2"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 	"log"
 	"os"
 	"time"
@@ -13,13 +18,41 @@ const (
 	database = "metrics"
 )
 
+type ResourceMetric struct {
+
+	// Disk Usage
+	DiskSize uint64  `json:"diskSize"`
+	DiskUsed uint64  `json:"diskUsed"`
+	DiskFree uint64  `json:"diskFree"`
+	DiskPct  float64 `json:"diskPercent"`
+
+	// Memory Usage
+	MemSize uint64 `json:"memSize"`
+	MemUsed uint64 `json:"memUsed"`
+	MemFree uint64 `json:"memFree"`
+
+	// SwapMemory Usage
+	SwapSize uint64 `json:"swapSize"`
+	SwapUsed uint64 `json:"swapUsed"`
+	SwapFree uint64 `json:"swapFree"`
+
+	// CPU Usage
+	CpuUsage float64 `json:"cpuUsage"`
+
+	// Network Usage
+	RxBytes uint64 `json:"rxBytes"`
+	TxBytes uint64 `json:"txBytes"`
+}
+
 func main() {
 	c := influxDBClient()
 	createDatabase(c)
 
 	for {
-		writeMetric(c)
-		fmt.Println("OK!")
+		m := collectMetric(c)
+		writeMetric(c, m)
+		j, _ := json.Marshal(m)
+		fmt.Println(string(j))
 		time.Sleep(1 * time.Second)
 	}
 
@@ -56,9 +89,49 @@ func createDatabase(c client.Client) *client.Response {
 	return response
 }
 
-func writeMetric(c client.Client) {
+func collectMetric(c client.Client) *ResourceMetric {
 	vm, _ := mem.VirtualMemory()
 	sm, _ := mem.SwapMemory()
+
+	ct, err := cpu.Times(false)
+	fmt.Println(ct)
+
+	if err != nil {
+		log.Fatalln("CPU INFO Error: ", err.Error())
+	}
+
+	du, err := disk.Usage("/")
+
+	if err != nil {
+		log.Fatalln("DISK USAGE Error: ", err.Error())
+	}
+
+	n, err := net.IOCounters(false)
+
+	if err != nil {
+		log.Fatalln("NET USAGE Error: ", err.Error())
+	}
+
+	m := new(ResourceMetric)
+
+	m.DiskSize = du.Total
+	m.DiskUsed = du.Used
+	m.DiskFree = du.Free
+	m.DiskPct = du.UsedPercent
+	m.MemSize = vm.Total
+	m.MemUsed = vm.Used
+	m.MemFree = vm.Free
+	m.SwapSize = sm.Total
+	m.SwapUsed = sm.Used
+	m.SwapFree = sm.Free
+	m.CpuUsage = ct[0].System
+	m.RxBytes = n[0].BytesRecv
+	m.TxBytes = n[0].BytesSent
+
+	return m
+}
+
+func writeMetric(c client.Client, m *ResourceMetric) {
 
 	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
 		Database:  database,
@@ -72,14 +145,7 @@ func writeMetric(c client.Client) {
 	}
 
 	tags := map[string]string{"hostname": hostname}
-	fields := map[string]interface{}{
-		"memsize":  vm.Total,
-		"memused":  vm.Used,
-		"memfree":  vm.Free,
-		"swapsize": sm.Total,
-		"swapused": sm.Used,
-		"swapfree": sm.Free,
-	}
+	fields := structs.Map(m)
 
 	pt, err := client.NewPoint("node_metrics", tags, fields, time.Now())
 
